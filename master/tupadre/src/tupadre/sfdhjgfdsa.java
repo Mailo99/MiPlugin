238d5ea5a1b01596781b71d2270b31999f048304
Mailo99 <mailo99.twitch@gmail.com>
2021-04-11 14:05:27 +0200

    ahora tu padre se llama pepe

--- master/tupadre/src/tupadre/sfdhjgfdsa.java
+++ master/tupadre/src/tupadre/sfdhjgfdsa.java
@@ -3,7 +3,7 @@ package tupadre;
 public class sfdhjgfdsa {
 	
 	public static void main(String[] args) {
-		System.out.println("HOLA PADRE");
+		System.out.println("HOLA PADRE se llama pepe");
 	}
 	
 }
